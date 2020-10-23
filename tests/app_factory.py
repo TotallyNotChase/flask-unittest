6aef56f2cb8fdb69b182a81be6376dcc4b721e1d
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-10-23 23:05:32 +0300

    Add testconfig TESTING option directly to create app

--- tests/app_factory.py
+++ tests/app_factory.py
@@ -3,8 +3,7 @@ from tests.flaskr.db import init_db
 
 # Create and set up the app
 # Do all the app config + pre launch setup here
-app = create_app()
-app.testing = True
+app = create_app({'TESTING': True})
 # Set up the database - this also drops existing tables
 with app.app_context():
     init_db()
