c0994e6014c77450bb2af910fd90a5600aab994f
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-10-31 23:16:56 +0400

    Make create_app sharable

--- tests/flaskr_test_example/conftest.py
+++ tests/flaskr_test_example/conftest.py
@@ -1,12 +1,10 @@
 import os
 import tempfile
 from typing import Iterator, Union
-from flask.globals import g
-
-from flask.testing import FlaskClient
 
 import flask_unittest
 from flask import Flask
+from flask.testing import FlaskClient
 
 from tests.flaskr import create_app
 from tests.flaskr.db import close_db, get_db
@@ -17,37 +15,40 @@ with open(os.path.join(os.path.dirname(__file__), "data.sql"), "rb") as f:
     _data_sql = f.read().decode("utf8")
 
 
+def _create_app(self):
+    """Create and configure a new app instance for each test."""
+    # create a temporary file to isolate the database for each test
+    db_fd, db_path = tempfile.mkstemp()
+    # create the app with common test config
+    app = create_app({"TESTING": True, "DATABASE": db_path})
+
+    # create the database and load test data
+    with app.app_context():
+        init_db()
+        get_db().executescript(_data_sql)
+
+        # Yield the app
+        '''
+        This can be outside the `with` block too, but we need to 
+        call `close_db` before exiting current context
+        Otherwise windows will have trouble removing the temp file
+        that doesn't happen on unices though, which is nice
+        '''
+        yield app
+
+        ## Close the db
+        close_db()
+
+    ## Cleanup temp file
+    os.close(db_fd)
+    os.remove(db_path)
+
+
 class TestBase(flask_unittest.AppClientTestCase):
     # The tests expect to use the session object - so set test_client to use cookies
     test_client_use_cookies = True
 
-    def create_app(self) -> Union[Flask, Iterator[Flask]]:
-        """Create and configure a new app instance for each test."""
-        # create a temporary file to isolate the database for each test
-        db_fd, db_path = tempfile.mkstemp()
-        # create the app with common test config
-        app = create_app({"TESTING": True, "DATABASE": db_path})
-
-        # create the database and load test data
-        with app.app_context():
-            init_db()
-            get_db().executescript(_data_sql)
-
-            # Yield the app
-            '''
-            This can be outside the `with` block too, but we need to 
-            call `close_db` before exiting current context
-            Otherwise windows will have trouble removing the temp file
-            that doesn't happen on unices though, which is nice
-            '''
-            yield app
-
-            ## Close the db
-            close_db()
-        
-        ## Cleanup temp file
-        os.close(db_fd)
-        os.remove(db_path)
+    create_app = _create_app
 
 
 class AuthActions(object):
--- tests/flaskr_test_example/test_db.py
+++ tests/flaskr_test_example/test_db.py
@@ -1,19 +1,18 @@
 import unittest
+from typing import Iterator, Union
 
 import sqlite3
+import flask_unittest
 from flask import Flask
 
 from tests.flaskr.db import get_db
-from tests.flaskr_test_example.conftest import TestBase
+from tests.flaskr_test_example.conftest import _create_app
 
+class TestDB(flask_unittest.AppTestCase):
 
-class TestDB(TestBase):
-    '''
-    NOTE: This inherits TestBase which is an AppClientTestCase
-    This test should ideally use an AppTestCase instead
-    This is omitted here for brevity
-    '''
-    def test_get_close_db(self, app: Flask, _):
+    create_app = _create_app
+
+    def test_get_close_db(self, app: Flask):
         with app.app_context():
             db = get_db()
             assert db is get_db()
